2d9ba5be549e4dd49daa6fb089e1574b5450c806
colsoncrim <colsonscrim@gmail.com>
2019-04-28 08:40:32 -0700

    First commit

---
+++ greeting.js
@@ -0,0 +1,26 @@
+exports.greetingEnglish = function(){
+ console.log("Hello");
+}
+
+exports.greetingJapanese = function(){
+ console.log("Konichiwa");
+}
+
+exports.greetingSpanish = function(){
+  console.log("Hola");
+}
+
+exports.greetingIcelandic = function(){
+  console.log("Hallo");
+}
+
+module.exports = {
+
+  conversationEnglishOne: function(){
+    console.log("Hello, how are you?");
+  },
+
+  conversationEnglishTwo: function(){
+    console.log("Good, how about you?");
+  }
+};
---
+++ main.js
@@ -0,0 +1,20 @@
+const greetings = require("./greeting.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+//  "Hola"
+greetings.greetingSpanish();
+
+//  "Hallo"
+greetings.greetingIcelandic();
+
+
+const conversation = require("./greeting.js");
+
+conversation.conversationEnglishOne();
+
+conversation.conversationEnglishTwo();
